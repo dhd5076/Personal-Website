19d4d846a53edd4e75787079c5de5f529ce7aa7b
Dylan Dunn <dhd5076@rit.edu>
2018-05-07 10:37:06 -0400

    Added location data

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var app = express();
 
 app.set('view engine', 'pug')
 app.set("views", path.join(__dirname, "views"));
+app.use(express.static('public'))
 app.use('/', routes);
 
 app.listen(8080, "localhost", () => {
